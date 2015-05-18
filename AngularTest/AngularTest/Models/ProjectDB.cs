632f9ed43ac8f8f86504dd8cc7317a68c3aed009
ahsan <ahsan@isoftgeeks.com>
2015-05-19 01:39:21 +0600

    set up and get the data

---
+++ AngularTest/AngularTest/Models/ProjectDB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AngularTest.Models
+{
+    public class ProjectDB:DbContext
+    {
+        public ProjectDB() : base("AngularTest") 
+        {
+ 
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}

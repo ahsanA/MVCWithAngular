eec740db0f5916700bae27434748cd59136bbafb
Ahsan <ahsan@proggasoft.com>
2015-06-01 19:18:59 +0600

    level 2 completed

--- CoreAngularLearning/app.js
+++ CoreAngularLearning/app.js
@@ -5,20 +5,57 @@
 		this.products = gems;
 	});
 	
+	app.controller('GalleryController', function(){
+		this.current = 0;
+		this.setCurrent = function(newGallery){
+		  this.current = newGallery || 0;
+		};
+	});
+	
+	app.controller('PanelController',function(){
+		this.tab = 1;
+		this.selectTab = function(setTab){
+			this.tab = setTab;
+		};
+		this.isSelected = function(checkTab){
+			return this.tab === checkTab;
+		};		
+	});
+	
 	var gems = [
 		{
 			name: 'Dodecahedron',
-			price: 2.95,
+			price: 2,
 			description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
 			canPurchase:true,
-			souldOut:false
+			souldOut:false,
+			images:[
+				{
+					full:'blue-gem - Copy.png',
+					thumb:'blue-gem-thumb - Copy.png'
+				},
+				{
+					full:'blue-gem.png',
+					thumb:'blue-gem-thumb.png'
+				}
+			]
 		},
 		{
-		name: 'Pentagonal gem',
-		price: 5.95,
-		description:'Lorem Ipsum is simply dummy text of the printing',
-		canPurchase:false,
-		souldOut:false
-	}
+			name: 'Pentagonal gem',
+			price: 5.95,
+			description:'Lorem Ipsum is simply dummy text of the printing',
+			canPurchase:false,
+			souldOut:false,
+			images:[
+				{
+					full:'green-gem - Copy.png',
+					thumb:'green-gem-thumb - Copy.png'
+				},
+				{
+					full:'green-gem.png',
+					thumb:'green-gem-thumb.png'
+				}
+			]
+		}
 	]
 })();
